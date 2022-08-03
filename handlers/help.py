b2936e97f4b38fe8b736b40c7da7a3cbdb0468ca
Pkginstallsteffen <107831585+Pkginstallsteffen@users.noreply.github.com>
2022-08-03 06:12:10 +0530

    Update help.py

--- handlers/help.py
+++ handlers/help.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 
 @Client.on_message(command("help") & filters.private & ~filters.group & ~filters.edited)
 async def help_cmd(client: Client, message: Message):
-    await message.reply_sticker("CAACAgUAAx0CZIiVngABAoCAYqWU-JzBZtfz14vr_DfDkJyy7X8AAjYGAAIsk1lUo7RMhQfOm28eBA")
+    
     await message.reply_photo(f"{START_IMG}", caption=f"""
 ❄ **ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs ɪɴ {BOT_NAME} :**
 
@@ -34,15 +34,15 @@ async def help_cmd(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "💫 sᴜᴩᴩᴏʀᴛ 💫", url=f"https://t.me/{SUPPORT_GROUP}"
+                        "💞 sᴜᴩᴩᴏʀᴛ 💞", url=f"https://t.me/{SUPPORT_GROUP}"
                     ),
                     InlineKeyboardButton(
-                        "❄ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​ ❄", url="https://github.com/AnonymousR1025/FallenMusic"
+                        "☠️ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​ ☠️", url="https://github.com/Pkginstallsteffen/Alexia-X-Steffen"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "✨ ᴄʟᴏsᴇ ✨", callback_data="close_play"
+                        "⚡ ᴄʟᴏsᴇ ⚡", callback_data="close_play"
                     )
                 ]
             ]
